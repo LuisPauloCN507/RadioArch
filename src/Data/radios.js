9dbd06a2fc6ae9f612a6ed3a43fcd946572c25c7
LuisPauloCN507 <luispaulocn.d3v507@gmail.com>
2026-05-07 09:03:10 -0300

    feat: add initial radio data list with Piauí stations and genres

---
+++ src/Data/radios.js
@@ -0,0 +1,50 @@
+export const radioList = [
+  {
+    id: 1,
+    name: "Jovem Pan FM",
+    city: "Teresina - PI",
+    url: "https://shaincast.com.br:7100/stream", // Exemplo, vamos validar os links depois
+    logo: "/logos/jovempan.png",
+    genre: "Pop/News",
+  },
+  {
+    id: 2,
+    name: "Meio Norte FM",
+    city: "Teresina - PI",
+    url: "https://9515.BrasilStream.com.br/stream",
+    logo: "/logos/meionorte.png",
+    genre: "Variedades",
+  },
+  {
+    id: 3,
+    name: "Rádio Liderança",
+    city: "Parnaíba - PI", // A Liderança tem várias sedes, coloquei Parnaíba como exemplo
+    url: "https://Cdn.BrasilStream.com.br/7144/stream",
+    logo: "/logos/lideranca.png",
+    genre: "Popular",
+  },
+  {
+    id: 4,
+    name: "Imperial FM",
+    city: "Pedro II - PI",
+    url: "https://streaming01.zas.com.br/8024/stream",
+    logo: "/logos/imperial.png",
+    genre: "Variedades",
+  },
+  {
+    id: 5,
+    name: "Rock FM (Foco)",
+    city: "Web Rádio",
+    url: "https://n0a.radiojar.com/8s7u9m3tqwzuv",
+    logo: "/logos/rock.png",
+    genre: "Rock",
+  },
+  {
+    id: 6,
+    name: "Electro Mix (Foco)",
+    city: "Web Rádio",
+    url: "https://icecast.unitedradio.it/Virgin.mp3", // Exemplo de eletrônica
+    logo: "/logos/eletronica.png",
+    genre: "Electronic",
+  },
+];
